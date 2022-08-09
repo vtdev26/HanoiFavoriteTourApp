49019f48557df616ccb79afc0ee11e400b9ab23b
vanthanh <s1thanhs2@gmail.com>
2022-08-09 23:31:51 +0700

    Initial Commit

---
+++ HanoiFavoriteTourApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HanoiFavoriteTourApp
+//
+//  Created by Nguyen Van Thanh on 09/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HanoiFavoriteTourApp/HanoiFavoriteTourAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HanoiFavoriteTourAppApp.swift
+//  HanoiFavoriteTourApp
+//
+//  Created by Nguyen Van Thanh on 09/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct HanoiFavoriteTourAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
